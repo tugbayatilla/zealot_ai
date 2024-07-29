971f06566eb7327997e73fed2b04f31c9dfae34e
Tugbay Atilla <tugbayatilla@gmail.com>
2024-07-29 15:53:13 +0200

    fix: reference in chroma

--- lib/chroma/ally_ai_chroma/AllyChroma.py
+++ lib/chroma/ally_ai_chroma/AllyChroma.py
@@ -5,7 +5,7 @@ from ally_ai_core import Settings
 from ally_ai_langchain import Embeddings
 from typing import Optional
 import logging
-from ChromaEmbeddingsVisualisation import ChromaEmbeddingsVisualisation
+from .ChromaEmbeddingsVisualisation import ChromaEmbeddingsVisualisation
 
 class AllyChroma(LangChainChroma):
     """
@@ -24,7 +24,7 @@ class AllyChroma(LangChainChroma):
             embeddings = Embeddings()
 
         self.ally_settings = settings
-        persist_directory = settings.pop('persist_directory')
+        persist_directory = settings.pop('persist_directory') if 'persist_directory' in settings else None
 
         if persist_directory is not None and persist_directory:
             logging.info(f"Loading local chroma using '{persist_directory}' directory.")
