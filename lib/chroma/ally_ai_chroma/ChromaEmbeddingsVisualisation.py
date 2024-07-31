b60c8d95348cf050b46b609a56a1a1f28c170b56
Tugbay Atilla <tugbayatilla@gmail.com>
2024-07-31 11:03:42 +0200

    feat: call returns pyplot Figure now

--- lib/chroma/ally_ai_chroma/ChromaEmbeddingsVisualisation.py
+++ lib/chroma/ally_ai_chroma/ChromaEmbeddingsVisualisation.py
@@ -1,8 +1,11 @@
 from typing import Literal, Optional
+
+from matplotlib.figure import Figure
 from .Chroma import Chroma
 from ally_ai_core.embeddings.EmbeddingsVisualisation import EmbeddingsVisualisation
 import logging
-logging.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s - %(message)s")
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s")
 logger = logging.getLogger(__name__)
 
 
@@ -11,20 +14,23 @@ class ChromaEmbeddingsVisualisation:
         logger.info("init is called.")
 
         self.chroma_client = chroma
-        all_embeddings = self.get_all_embeddings(limit=limit)
-        self._visualisation = EmbeddingsVisualisation(all_embeddings)
+        self.document_embeddings, self.documents = self.get_documents_and_embeddings(
+            limit=limit)
+        self.visualisation = EmbeddingsVisualisation(
+            self.document_embeddings)
 
-    def get_all_embeddings(self, limit: Optional[int] = None):
-        logger.info("fetching embeddings is starting...")
+    def get_documents_and_embeddings(self, limit: Optional[int] = None):
+        include = ['embeddings', 'documents']
+        logger.info(f"getting data '{include}' from chroma client")
 
-        collection = self.chroma_client.get(
-            limit=limit, include=['embeddings'])
-        embeddings = collection['embeddings']
-        
-        logger.info(f"fetching embeddings is done. Len:{len(embeddings)}")
-        return embeddings
+        collection = self.chroma_client.get(limit=limit, include=include)
+        document_embeddings = collection['embeddings']
+        documents = collection['documents']
 
-    def __call__(self, query, search_type: Literal['similarity', 'mmr', 'similarity_score_threshold']='mmr', **kwargs):
+        logger.info(f"total documents: {len(documents)}")
+        return (document_embeddings, documents)
+
+    def __call__(self, query, search_type: Literal['similarity', 'mmr', 'similarity_score_threshold'] = 'mmr', **kwargs) -> Figure:
         """
         - Makes similariy search and retrieves documents
         - Displays
@@ -33,14 +39,14 @@ class ChromaEmbeddingsVisualisation:
             - Marks retrieved documents with 'green circle'
 
         """
-        retrieved_documents = self.chroma_client.search(query=query, search_type=search_type, **kwargs)
+        logger.info(f"retrieving documents. Query: {query}")
+        retrieved_documents = self.chroma_client.search(
+            query=query, search_type=search_type, **kwargs)
 
+        logger.info(f"embedding the query. Query: {query}")
         query_embeddings = self.chroma_client.embeddings.embed_query(query)
-        doc_embeddings = self.chroma_client.embeddings.embed_documents(
-            [doc.page_content for doc in retrieved_documents]) 
-        # unfortunatelly we have to re-embed documents because we cannot fetch embedding with framework, does not support :(
 
-        self._visualisation(
+        return self.visualisation(
             title=query,
             query_embeddings=query_embeddings,
-            document_embeddings=doc_embeddings)
+            document_embeddings=retrieved_documents)
