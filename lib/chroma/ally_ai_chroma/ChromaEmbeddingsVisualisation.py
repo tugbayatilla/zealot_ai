f127f7a9bc7bdbfe52d4ffc37bee7e1ace504571
Tugbay Atilla <tugbayatilla@gmail.com>
2024-07-31 14:23:30 +0200

    feat: search and visualise_search are available

--- lib/chroma/ally_ai_chroma/ChromaEmbeddingsVisualisation.py
+++ lib/chroma/ally_ai_chroma/ChromaEmbeddingsVisualisation.py
@@ -38,9 +38,40 @@ class ChromaEmbeddingsVisualisation:
         """ + self.visualise.__doc__
 
         return self.visualise(query=query, search_type=search_type, **kwargs)
-    
 
-    def visualise(self, query, search_type: Literal['similarity', 'mmr', 'similarity_score_threshold'] = 'mmr', **kwargs) -> Figure:
+    def search(self, query:str, search_type: Literal['similarity', 'mmr', 'similarity_score_threshold'] = 'mmr', **kwargs) -> dict:
+        """
+        Output structure:
+        ```python
+        {
+            'query': Any,
+            'query_embeddings': Any,
+            'retrieved_documents': Any,
+            'retrieved_documents_embeddings': Any
+        }
+        ```
+        
+        """
+        logger.info(f"retrieving documents. Query: '{query}'")
+        retrieved_documents = self.chroma_client.search(
+            query=query, search_type=search_type, **kwargs)
+
+        logger.info(
+            f"embedding retrieved documents. Len:{len(retrieved_documents)}")
+        retrieved_documents_embeddings = self.chroma_client.embeddings.embed_documents(
+            [doc.page_content for doc in retrieved_documents])
+
+        logger.info(f"embedding the query. Query: '{query}'")
+        query_embeddings = self.chroma_client.embeddings.embed_query(query)
+
+        return {
+            'query': query,
+            'query_embeddings': query_embeddings,
+            'retrieved_documents': retrieved_documents,
+            'retrieved_documents_embeddings': retrieved_documents_embeddings
+        }
+
+    def visualise(self, query:str, search_type: Literal['similarity', 'mmr', 'similarity_score_threshold'] = 'mmr', **kwargs) -> dict:
         """
         - Makes similariy search and retrieves documents
         - Displays
@@ -49,18 +80,37 @@ class ChromaEmbeddingsVisualisation:
             - Marks retrieved documents with 'green circle'
 
         """
-        logger.info(f"retrieving documents. Query: '{query}'")
-        retrieved_documents = self.chroma_client.search(
-            query=query, search_type=search_type, **kwargs)
         
-        logger.info(f"embedding retrieved documents. Len:{len(retrieved_documents)}")
-        retrieved_documents_embeddings = self.chroma_client.embeddings.embed_documents(
-            [doc.page_content for doc in retrieved_documents]) 
-        
-        logger.info(f"embedding the query. Query: '{query}'")
-        query_embeddings = self.chroma_client.embeddings.embed_query(query)
+        search_results = self.search(query=query, search_type=search_type, **kwargs)
 
-        return self._embedding_visualisation_class(
+        figure = self._embedding_visualisation_class(
             title=query,
-            query_embeddings=query_embeddings,
-            document_embeddings=retrieved_documents_embeddings)
+            query_embeddings=search_results['query_embeddings'],
+            document_embeddings=search_results['retrieved_documents_embeddings']
+        )
+
+        return {
+            'figure': figure,
+            **search_results
+        }
+
+    def visualise_search(self, search_results: dict) -> dict:
+        """
+        - Makes similariy search and retrieves documents
+        - Displays
+            - All documents as gray dot
+            - Marks query with 'red X'
+            - Marks retrieved documents with 'green circle'
+
+        """
+        
+        figure = self._embedding_visualisation_class(
+            title=search_results['query'],
+            query_embeddings=search_results['query_embeddings'],
+            document_embeddings=search_results['retrieved_documents_embeddings']
+        )
+        
+        return {
+            'figure': figure,
+            **search_results
+        }
