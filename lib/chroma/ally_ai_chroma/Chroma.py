fae1d23b98120a6626311871ecd606c6d8879602
Tugbay Atilla <tugbayatilla@gmail.com>
2024-07-31 13:45:48 +0200

    feat: disable telemetry while using persist directory

--- lib/chroma/ally_ai_chroma/Chroma.py
+++ lib/chroma/ally_ai_chroma/Chroma.py
@@ -25,17 +25,19 @@ class Chroma(LangChainChroma):
         self.ally_settings = settings
         persist_directory = settings.pop('persist_directory') if 'persist_directory' in settings else None
 
+        chroma_settings = ChromaSettings(anonymized_telemetry=False)
         if persist_directory is not None and persist_directory:
             logging.info(f"Loading chroma using '{persist_directory}' directory.")
             super().__init__(persist_directory=persist_directory,
                              embedding_function=embeddingModel,
                              create_collection_if_not_exists=False,
+                             client_settings=chroma_client
                              **kwargs)
         else:
             logging.info(f"Loading chroma client using '{settings['host']}' as host.")
             chroma_client = chromadb.HttpClient(
                 **settings,
-                settings=ChromaSettings(anonymized_telemetry=False))
+                settings=chroma_settings)
             super().__init__(client=chroma_client,
                              embedding_function=embeddingModel,
                              create_collection_if_not_exists=False,
