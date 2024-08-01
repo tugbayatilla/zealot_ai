f8698aaaeb81b02176004b1b6aba196a0d976734
Tugbay Atilla <tugbayatilla@gmail.com>
2024-08-01 15:10:04 +0200

    chore: setup dev environment script

--- setup_dev.sh
+++ setup_dev.sh
@@ -7,5 +7,9 @@ which python3
 
 pip install -r ./requirements.txt
 
+#copy this path to .env for vscode
+cat > .env << EOL
 PYTHONPATH=./lib/core:./lib/langchain:./lib/llamaindex:./lib/ally::./lib/chroma
-export PYTHONPATH
+LLM__API_KEY=
+EMBEDDINGS__API_KEY=
+EOL
