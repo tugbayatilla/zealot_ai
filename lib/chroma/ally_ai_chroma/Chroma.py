c57ca40cfb5d13d24ce573cbb1f4ff64f6082aa6
Tugbay Atilla <tugbayatilla@gmail.com>
2024-09-26 10:43:00 +0200

    feat: chroma logs init stages

--- lib/chroma/ally_ai_chroma/Chroma.py
+++ lib/chroma/ally_ai_chroma/Chroma.py
@@ -7,50 +7,73 @@ from ally_ai_langchain import EmbeddingModel
 from typing import Optional
 import logging
 
+from ally_ai_core.Decorators import logstep
+
+
 class Chroma(LangChainChroma):
     """
     Interited from LangChain Chroma
     """
+
     ally_settings: Settings = None
 
-    def __init__(self,
-                 settings: Optional[Settings] = None,
-                 embeddingModel: Optional[EmbeddingModel] = None,
-                 **kwargs) -> None:
+    def __init__(
+        self,
+        settings: Optional[Settings] = None,
+        embeddingModel: Optional[EmbeddingModel] = None,
+        **kwargs,
+    ) -> None:
 
         if settings is None:
-            settings = Settings(section='chromadb')
+            settings = Settings(section="chromadb")
         if embeddingModel is None:
             embeddingModel = EmbeddingModel()
 
         self.ally_settings = settings
-        persist_directory = settings.pop('persist_directory') if 'persist_directory' in settings else None
+        persist_directory = (
+            settings.pop("persist_directory")
+            if "persist_directory" in settings
+            else None
+        )
 
         if persist_directory is not None and persist_directory:
-            logging.info(f"Loading chroma using '{persist_directory}' directory.")
-            super().__init__(persist_directory=persist_directory,
-                             embedding_function=embeddingModel,
-                             create_collection_if_not_exists=False,
-                             **kwargs)
+            self._init_persist_directory(embeddingModel, persist_directory, **kwargs)
         else:
-            logging.info(f"Loading chroma client using '{settings['host']}' as host.")
-            chroma_client = chromadb.HttpClient(
-                **settings,
-                settings=ChromaSettings(anonymized_telemetry=False))
-            super().__init__(client=chroma_client,
-                             embedding_function=embeddingModel,
-                             create_collection_if_not_exists=False,
-                             **kwargs)
+            self._init_http_client(settings, embeddingModel, **kwargs)
 
-    def query(self, 
+    @logstep(message="Chroma-Init-PersistDir")
+    def _init_persist_directory(self, embeddingModel, persist_directory, **kwargs):
+        super().__init__(
+            persist_directory=persist_directory,
+            embedding_function=embeddingModel,
+            create_collection_if_not_exists=False,
+            **kwargs,
+        )
+
+    @logstep(message="Chroma-Init-HttpClient")
+    def _init_http_client(self, settings, embeddingModel, **kwargs):
+        chroma_client = chromadb.HttpClient(
+            **settings, settings=ChromaSettings(anonymized_telemetry=False)
+        )
+        super().__init__(
+            client=chroma_client,
+            embedding_function=embeddingModel,
+            create_collection_if_not_exists=False,
+            **kwargs,
+        )
+
+    def query(
+        self,
         query_texts: Optional[OneOrMany[str]] = None,
         n_results: int = 10,
-        include: chromadb.Include = ["metadatas", "documents", "distances"]):
+        include: chromadb.Include = ["metadatas", "documents", "distances"],
+    ):
 
-        query_embeddings = [self._embedding_function.embed_query(query_text) for query_text in query_texts]
-    
+        query_embeddings = [
+            self._embedding_function.embed_query(query_text)
+            for query_text in query_texts
+        ]
+
         return self._collection.query(
-            query_embeddings=query_embeddings, 
-            n_results=n_results,
-            include=include
+            query_embeddings=query_embeddings, n_results=n_results, include=include
         )
--- tests/chroma/test_chroma.py
+++ tests/chroma/test_chroma.py
@@ -4,34 +4,51 @@ import pytest
 from ally_ai_core import Settings
 from ally_ai_langchain import EmbeddingModel
 
+
 @pytest.fixture
 def instance():
-    path='./tests/chroma/app-settings.yaml'
+    path = "./tests/chroma/app-settings.yaml"
 
-    chroma_settings = Settings(section='chromadb', path=path)
-    embedding_settings = Settings(section='embeddings', path=path)
-    
+    chroma_settings = Settings(section="chromadb", path=path)
+    embedding_settings = Settings(section="embeddings", path=path)
+
     embeddingModel = EmbeddingModel(settings=embedding_settings)
     return Chroma(settings=chroma_settings, embeddingModel=embeddingModel)
 
+
 @pytest.mark.integration
 def test_query_document(instance):
-    results = instance.query('what is an ally?', n_results=2, include=['documents'])
-    retrieved_documents = results['documents'][0]
-    
+    results = instance.query("what is an ally?", n_results=2, include=["documents"])
+    retrieved_documents = results["documents"][0]
+
     assert len(retrieved_documents) == 2
+
 
 @pytest.mark.integration
 def test_query_embeddings(instance):
-    results = instance.query('what is an ally?', n_results=2, include=['embeddings'])
-    retrieved_documents = results['embeddings'][0]
-    
+    results = instance.query("what is an ally?", n_results=2, include=["embeddings"])
+    retrieved_documents = results["embeddings"][0]
+
     assert len(retrieved_documents) == 2
+
 
 @pytest.mark.integration
 def test_multiple_query(instance):
-    queries = ['what is an ally?', 'do we need it?']
-    results = instance.query(query_texts=queries, n_results=2, include=['documents'])
-    retrieved_documents = results['documents'][0]
-    
+    queries = ["what is an ally?", "do we need it?"]
+    results = instance.query(query_texts=queries, n_results=2, include=["documents"])
+    retrieved_documents = results["documents"][0]
+
     assert len(retrieved_documents) == 2
+
+
+@pytest.mark.integration
+def test_availability():
+    path = "./tests/chroma/app-settings.yaml"
+
+    chroma_settings = Settings(section="chromadb_unavailable", path=path)
+    embedding_settings = Settings(section="embeddings", path=path)
+
+    embeddingModel = EmbeddingModel(settings=embedding_settings)
+    with pytest.raises(Exception) as e_info:
+        _ = Chroma(settings=chroma_settings, embeddingModel=embeddingModel)
+    print(e_info)
