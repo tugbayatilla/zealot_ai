3468ac30fe51d22e602f6cf6fa25d21d9e493fb4
Tugbay Atilla <tugbayatilla@gmail.com>
2024-10-11 17:59:07 +0200

    feat: register modules

--- lib/core/ally_ai_core/utils.py
+++ lib/core/ally_ai_core/utils.py
@@ -2,23 +2,8 @@ import logging
 import os
 import sys
 
-def get_loglevel() -> int:
-    """
-    returns log level 
-        if LOG_LEVEL is in environment variables
-    or
-        'INFO'
-    """
-    _default_level = 'INFO'
-    LOG_LEVEL = os.environ.get('LOG_LEVEL', _default_level).upper()
-    if LOG_LEVEL == '':
-        LOG_LEVEL = _default_level
 
-    return logging._nameToLevel[LOG_LEVEL]
-
-LOG_LEVEL = get_loglevel()
-
-def get_value_or_default(key:str, default:any, **kwargs):
+def get_value_or_default(key: str, default: any, **kwargs):
     """
     returns kwargs value or default
         if key is in kwargs
@@ -28,10 +13,31 @@ def get_value_or_default(key:str, default:any, **kwargs):
     return kwargs[key] if key in kwargs else default
 
 
-def load_test_logger(level = logging.INFO):
-  test_logger = logging.getLogger()
-  test_logger.setLevel(level=level)
-  console_handler = logging.StreamHandler(stream=sys.stdout)
-  console_handler.formatter = logging.Formatter('::> %(name)s - %(levelname)s - %(message)s')
-  test_logger.handlers.clear()
-  test_logger.addHandler(console_handler)
+def load_test_logger(level=logging.INFO):
+    test_logger = logging.getLogger()
+    test_logger.setLevel(level=level)
+    console_handler = logging.StreamHandler(stream=sys.stdout)
+    console_handler.formatter = logging.Formatter(
+        "::> %(name)s - %(levelname)s - %(message)s"
+    )
+    test_logger.handlers.clear()
+    test_logger.addHandler(console_handler)
+
+
+def register_modules(file: str, *paths):
+    """
+    Register modules in sys path.
+    If it is already registered, it will be skipped
+
+    Args:
+        file: __file__
+        paths: 'src', 'tools', 'module1'
+    """
+    src_dir = os.path.dirname(os.path.abspath(file))
+    if not src_dir in sys.path:
+        sys.path.insert(0, src_dir)
+
+    for name in paths:
+        tools_dir = os.path.join(src_dir, name)
+        if not tools_dir in sys.path:
+            sys.path.insert(0, tools_dir)
