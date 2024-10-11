793f4abbc07f746c07556d3fa2d6468debf2170a
Tugbay Atilla <tugbayatilla@gmail.com>
2024-10-11 13:54:14 +0200

    feat: decorator lowercase namespace is available

--- lib/core/ally_ai_core/__init__.py
+++ lib/core/ally_ai_core/__init__.py
@@ -2,13 +2,7 @@ from . import utils as Utils
 from .settings.Settings import Settings
 from . import errors as Errors
 from . import Decorators
+from . import Decorators as decorators
 from . import ContextManagers
 
-__all__ = [
-    'Utils',
-    'Settings',
-    'Errors',
-    'Decorators',
-    'ContextManagers'
-
-]
+__all__ = ["Utils", "Settings", "Errors", "Decorators", "decorators", "ContextManagers"]
