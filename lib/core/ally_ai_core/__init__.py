b07999da666f14f89e2d63b72700e71742f0e4f5
Tugbay Atilla <tugbayatilla@gmail.com>
2024-10-15 15:33:45 +0200

    fix: back again to small d

--- lib/core/ally_ai_core/__init__.py
+++ lib/core/ally_ai_core/__init__.py
@@ -1,7 +1,7 @@
 from . import utils
 from .settings.Settings import Settings
 from . import errors
-from . import decorators as decorators
+from . import decorators
 from . import context_managers
 
 __all__ = ["utils", "Settings", "errors", "decorators", "context_managers"]
