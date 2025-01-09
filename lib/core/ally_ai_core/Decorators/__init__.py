3e06eac698fc9b11678f808dccb530df0343e3e1
Tugbay Atilla <tugbayatilla@gmail.com>
2025-01-09 11:40:23 +0100

    fix: add tracer to init

--- lib/core/ally_ai_core/Decorators/__init__.py
+++ lib/core/ally_ai_core/Decorators/__init__.py
@@ -1,4 +1,6 @@
+from .trace import trace, Tracer, default_tracer, default_logger
 from .logged import logged, Logged
 from .timed import timed
 
-__all__ = ["logged", "timed", "Logged"]
+
+__all__ = ["logged", "timed", "Logged", trace, Tracer, default_tracer, default_logger]
